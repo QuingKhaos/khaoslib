591f1f232af6c1a537214143b250488f5982ca53
Em Karisch <em@khaos.solutions>
2025-09-19 13:06:32 +0200

    docs: improve type hinting

--- sprites.lua
+++ sprites.lua
@@ -26,6 +26,7 @@ local khaoslib_sprites = {}
 --- @param sprites T The sprite or animation or a table of them.
 --- @param fn fun(sprite: khaoslib_sprites.Animation|khaoslib_sprites.Sprite): nil A function that takes a sprite or animation copy and applies modifications to it.
 --- @return T copy A copy of the given animation or sprite or a table of them with the modifications applied.
+--- @nodiscard
 function khaoslib_sprites.traverse(sprites, fn)
   if type(sprites) ~= "table" then error("sprites parameter: Expected table, got " .. type(sprites), 3) end
   --- @cast sprites khaoslib_sprites.AnimationAll|khaoslib_sprites.SpriteAll
@@ -51,8 +52,8 @@ function khaoslib_sprites.traverse(sprites, fn)
   elseif sprites.layers then
     copy.layers = khaoslib_sprites.traverse(sprites.layers, fn)
   elseif sprites.filename then
-    copy = util.table.deepcopy(sprites) --- @cast copy khaoslib_sprites.Animation|khaoslib_sprites.Sprite
-    fn(copy)
+    copy = util.table.deepcopy(sprites)
+    fn(copy --[[@as khaoslib_sprites.Animation|khaoslib_sprites.Sprite]])
   else
     for _, sprite in ipairs(sprites) do
       table.insert(copy, khaoslib_sprites.traverse(sprite, fn))
@@ -67,6 +68,7 @@ end
 --- @param sprites T The animation or sprite or a table of them.
 --- @param replacements {[string]: string} A table where the key is the original filename and the value is the new filename.
 --- @return T copy A copy of the given animation or sprite or a table of them with the replaced filenames.
+--- @nodiscard
 function khaoslib_sprites.replace(sprites, replacements)
   if type(replacements) ~= "table" then error("replacements parameter: Expected table, got " .. type(replacements), 2) end
 
@@ -87,6 +89,7 @@ end
 --- @param sprites T The animation or sprite or a table of them.
 --- @param tint data.Color A color table, e.g. {r=1, g=0.5, b=0, a=0.5}
 --- @return T copy A copy of the given animation or sprite or a table of them with the tint applied.
+--- @nodiscard
 function khaoslib_sprites.tint(sprites, tint)
   if type(tint) ~= "table" then error("tint parameter: Expected table, got " .. type(tint), 2) end
 
